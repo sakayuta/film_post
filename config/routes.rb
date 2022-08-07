2537841224c9081feea64f49fc67d5535dcce15f
EC2 Default User <ec2-user@ip-172-31-11-35.ap-northeast-1.compute.internal>
2022-08-07 05:24:55 +0000

    Add_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # user用
+# user用
 # URL /customers/sign_in ...
 devise_for :users,skip: [:passwords], controllers: {
   registrations: "user/registrations",
@@ -11,5 +11,28 @@ devise_for :users,skip: [:passwords], controllers: {
 devise_for :admin,skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
+
+ #user側のルーティング設定
+ scope module: :user do
+   root to: "homes#top"
+   get '/about' => 'homes#about', as: 'about'
+   resources :films do
+     resource :favorites, only: [:create, :destroy]
+     resources :film_comments, only: [:create, :destroy]
+   end
+
+   get '/unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
+   get '/withdraw' => 'users#withdraw', as: 'withdraw'
+   resources :users, only: [:show, :edit, :update]
+ end
+
+ #管理者側のルーティング設定
+ namespace :admin do
+   root to: "homes#top"
+   resources :genres, only: [:index, :create, :edit, :update, :destroy]
+   resources :users, only: [:index, :show, :edit, :update]
+ end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
