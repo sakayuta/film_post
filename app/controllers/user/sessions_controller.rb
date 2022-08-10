f95c4581dc73e1d45a1e4c792e27400e7c11c47f
EC2 Default User <ec2-user@ip-172-31-11-35.ap-northeast-1.compute.internal>
2022-08-10 05:04:19 +0000

    Add_after-sign-in/out-path

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -24,4 +24,13 @@ class User::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def after_sign_in_path_for(resource)
+      user_path(current_user)
+  end
+
+  def after_sign_out_path_for(resource)
+      about_path
+  end
+
 end
