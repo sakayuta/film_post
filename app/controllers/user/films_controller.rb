bac30ff54483f795acb47170d17b4b94a2fd9c05
EC2 Default User <ec2-user@ip-172-31-11-35.ap-northeast-1.compute.internal>
2022-08-11 07:49:58 +0000

    Add_controllers-user-films

--- app/controllers/user/films_controller.rb
+++ app/controllers/user/films_controller.rb
@@ -1,2 +1,56 @@
 class User::FilmsController < ApplicationController
+
+  def new
+    @film = Film.new
+  end
+
+  def create
+    film = Film.new(film_params)
+    film.user_id = current_user.id
+    if film.save
+      redirect_to film_path(film.id), notice: "投稿できました！"
+    else
+      render :new
+    end
+  end
+
+  def index
+    @film = Film.all
+    @user = current_user
+  end
+
+  def show
+    @film = Film.find(params[:id])
+    @user = @film.user
+  end
+
+  def edit
+    @film = Film.find(params[:id])
+    if @film.user != current_user
+       redirect_to films_path
+    end
+  end
+
+  def update
+    film = Film.find(params[:id])
+    if film.update(film_params)
+      redirect_to film_path(film.id), notice: "編集できました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    film = Film.find(params[:id])
+    film.destroy
+    redirect_to films_path
+  end
+
+
+  private
+
+  def film_params
+    params.require(:film).permit(:title, :body, :image)
+  end
+
 end
