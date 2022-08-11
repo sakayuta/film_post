a49cb4e47054a0e21ae12eba4aae3c1d95c51f34
EC2 Default User <ec2-user@ip-172-31-11-35.ap-northeast-1.compute.internal>
2022-08-11 06:05:28 +0000

    Add_user-edit

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,29 +1,32 @@
 class User::UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update]
-  
+
   def show
     @user = User.find(params[:id])
     @films = @user.films
   end
-  
+
   def index
      @users = User.all
      @user = current_user
   end
-  
-  
+
+
   def edit
      @user = User.find(params[:id])
   end
-  
+
   def update
+  
     if @user.update(user_params)
       redirect_to user_path(@user.id), notice: "編集が完了しました"
     else
       render :edit
     end
   end
-  
+
+  private
+
   def user_params
     params.require(:user).permit(:name, :name_kana, :sex, :age_group, :email ,:profile_image)
   end
@@ -34,5 +37,5 @@ class User::UsersController < ApplicationController
       redirect_to user_path(current_user)
     end
   end
-  
+
 end
