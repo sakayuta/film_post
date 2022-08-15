225a919d0b0456ffcb89f35bcdf8373534d35b8c
EC2 Default User <ec2-user@ip-172-31-11-35.ap-northeast-1.compute.internal>
2022-08-15 03:21:39 +0000

    Add_controllers-favorites

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -1,2 +1,17 @@
 class User::FavoritesController < ApplicationController
+
+  def create
+    @film = Film.find(params[:film_id])
+    favorite = current_user.favorites.new(film_id: @film.id)
+    favorite.save
+    #redirect_to request.referer
+  end
+
+  def destroy
+    @film = Film.find(params[:film_id])
+    favorite = current_user.favorites.find_by(film_id: @film.id)
+    favorite.destroy
+   # redirect_to request.referer
+  end
+
 end
