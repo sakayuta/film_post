849e4be9681ddf543b0506361ccd48772393b543
EC2 Default User <ec2-user@ip-172-31-11-35.ap-northeast-1.compute.internal>
2022-08-15 03:10:50 +0000

    Add_controllers-film_comments

--- app/controllers/user/film_comments_controller.rb
+++ app/controllers/user/film_comments_controller.rb
@@ -1,2 +1,28 @@
 class User::FilmCommentsController < ApplicationController
+  
+  def create
+    @film = Film.find(params[:film_id])
+    @film_comment = FilmComment.new(film_comment_params)
+    @film_comment.user_id = current_user.id
+    @film_comment.film_id = @film.id
+    @film_comment.save
+    #redirect_to request.referer
+  end
+
+  def destroy
+    @film = Film.find(params[:film_id])
+    @film_comment=FilmComment.find_by(id: params[:id], film_id: params[:film_id])
+    @film_comment.destroy
+    #redirect_to request.referer
+  end
+
+
+
+  private
+
+  def film_comment_params
+    params.require(:film_comment).permit(:comment)
+  end
+  
+  
 end
