a64fc02dbc31e811e626cfb79354a94fe1e722ea
EC2 Default User <ec2-user@ip-172-31-11-35.ap-northeast-1.compute.internal>
2022-08-18 13:00:51 +0000

    Fix_検索機能

--- app/controllers/user/searches_controller.rb
+++ app/controllers/user/searches_controller.rb
@@ -2,7 +2,16 @@ class User::SearchesController < ApplicationController
    before_action :authenticate_user!
 
   def search
-    @films = Film.search(params[:keyword]).page(params[:page])
+    films = Film.search(params[:keyword]).page(params[:page]) 
+    if params[:latest]
+      @films = films.latest.page(params[:page])
+    elsif params[:old]
+      @films = films.old.page(params[:page])
+    elsif params[:star_count]
+      @films = films.star_count.page(params[:page])
+    else
+      @films = films
+    end
     @keyword = params[:keyword]
   end
 end
